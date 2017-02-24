f4356280331934a85ea7062dd9f18b97c57529bb
Aryan Sharma <Aryan@aryans-macbook-air.local>
2017-02-24 16:05:58 +0530

    added speech recognition, font awsome

--- Pods/Target Support Files/Pods-WebApp_Apple/Pods-WebApp_Apple-frameworks.sh
+++ Pods/Target Support Files/Pods-WebApp_Apple/Pods-WebApp_Apple-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/FontAwesome.swift/FontAwesome_swift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Toaster/Toaster.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/FontAwesome.swift/FontAwesome_swift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Toaster/Toaster.framework"
 fi
--- WebApp_Apple/ViewController.swift
+++ WebApp_Apple/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 import Toaster
 import SystemConfiguration
 import UserNotifications
+import Speech
+import FontAwesome_swift
 
-class ViewController: UIViewController, UIWebViewDelegate,UIGestureRecognizerDelegate, UIScrollViewDelegate, UITextFieldDelegate {
+class ViewController: UIViewController, UIWebViewDelegate,UIGestureRecognizerDelegate, UIScrollViewDelegate, UITextFieldDelegate, SFSpeechRecognizerDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
@@ -23,6 +25,7 @@ class ViewController: UIViewController, UIWebViewDelegate,UIGestureRecognizerDel
     @IBOutlet weak var incogView: UIView!
     @IBOutlet weak var mainStackView: UIStackView!
     
+    @IBOutlet weak var micButton: UIButton!
     @IBOutlet weak var AddressBarConstraint: NSLayoutConstraint!
     
     var isFirstLoad = true
@@ -30,6 +33,11 @@ class ViewController: UIViewController, UIWebViewDelegate,UIGestureRecognizerDel
     var swipeRight: UISwipeGestureRecognizer!
     var isGrantedNotificationAccess:Bool = false
     let defaults = UserDefaults.standard
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-IN"))  //1
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
 
     
     
@@ -48,6 +56,39 @@ class ViewController: UIViewController, UIWebViewDelegate,UIGestureRecognizerDel
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        micButton.titleLabel?.font = UIFont.fontAwesome(ofSize: 15)
+        micButton.setTitle(String.fontAwesomeIcon(name: .microphone), for: .normal)
+        micButton.isEnabled = false
+        
+        speechRecognizer?.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.micButton.isEnabled = isButtonEnabled
+            }
+        }
+
         
         incogView.isHidden = true
         view.layer.cornerRadius = 10
@@ -409,7 +450,7 @@ class ViewController: UIViewController, UIWebViewDelegate,UIGestureRecognizerDel
     }
     
     
-    // MARK: TextField Delegates
+    // MARK: Delegates
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         addressTextField.resignFirstResponder()
@@ -421,7 +462,6 @@ class ViewController: UIViewController, UIWebViewDelegate,UIGestureRecognizerDel
         addressTextField.selectedTextRange = addressTextField.textRange(from: addressTextField.beginningOfDocument, to: addressTextField.endOfDocument)
     }
     
-    // MARK: WebView Delegates
     
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         addressTextField.text = webView.request?.url?.absoluteString
@@ -468,18 +508,114 @@ class ViewController: UIViewController, UIWebViewDelegate,UIGestureRecognizerDel
         addressTextField.text = webView.request?.url?.absoluteString
     }
     
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            micButton.isEnabled = true
+        } else {
+            micButton.isEnabled = false
+        }
+    }
+    
     // MARK: Buttons
     
+    @IBAction func micBtnPressed(_ sender: UIButton) {
+        print("mic btn pressed")
+        
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            micButton.isEnabled = false
+            micButton.setTitle(String.fontAwesomeIcon(name: .microphone), for: .normal)
+//            microphoneButton.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            micButton.setTitle(String.fontAwesomeIcon(name: .stopCircleO), for: .normal)
+
+//            microphoneButton.setTitle("Stop Recording", for: .normal)
+        }
+    }
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+//                print(result?.bestTranscription.formattedString)
+                self.addressTextField.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+//                print(result?.bestTranscription.formattedString)
+                if result != nil {
+                    self.loadUrl(addUrl: (result?.bestTranscription.formattedString)!)
+                }
+
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.micButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+            print("audio engine started")
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+//        addressTextField.text = "Say something, I'm listening!"
+        
+    }
+    
     func stopBtn() {
-        StopRefreshBtn.setImage(nil, for: .normal)
-        StopRefreshBtn.setTitle("X", for: .normal)
-        StopRefreshBtn.setTitleColor(UIColor.black, for: .normal)
+        StopRefreshBtn.setTitle(String.fontAwesomeIcon(name: .stopCircleO), for: .normal)
+        StopRefreshBtn.setTitleColor(UIColor.white, for: .normal)
     }
     
     func refreshBtn() {
-        StopRefreshBtn.setTitle(nil, for: .normal)
-        StopRefreshBtn.setImage(UIImage(named: "refreshIcon"), for: .normal)
-    }
+        StopRefreshBtn.titleLabel?.font = UIFont.fontAwesome(ofSize: 17)
+        StopRefreshBtn.setTitle(String.fontAwesomeIcon(name: .refresh), for: .normal)
+}
     
     @IBAction func BackBtnPressed(_ sender: Any) {
         clearBtnPressed(Any.self)
